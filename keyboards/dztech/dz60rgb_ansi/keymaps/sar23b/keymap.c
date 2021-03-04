bb06f1d6cbff1b4dec3d757e020848da7b986b93
sar <git.sar@coors-online.de>
2021-03-04 22:56:01 +0100

    add media play

--- keyboards/dztech/dz60rgb_ansi/keymaps/sar23b/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/sar23b/keymap.c
@@ -16,9 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
 		_______, KC_MPRV, KC_UP,   KC_MNXT, _______, _______, _______, _______, KC_UP,   _______, _______, KC_HOME, KC_END,  KC_PSCR,
-		KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, _______,
-		_______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  _______,
-		_______, _______, _______, _______, _______, _______, _______, _______
+		KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, KC_MPLY,
+		_______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  KC_MPLY,
+		_______, _______, _______, _______, KC_MPLY, _______, KC_MPLY, _______
 		),
 	[2] = LAYOUT_60_ansi(
 		KC_ESC,  RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLEP,
