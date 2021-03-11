549b5b6956512a5c82ae9da2301738a8e5d004ca
sar <git.sar@coors-online.de>
2021-03-11 20:37:59 +0100

    move backspace down

--- keyboards/dztech/dz60rgb_ansi/keymaps/sar23b/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/sar23b/keymap.c
@@ -7,11 +7,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_60_ansi(
-		KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,    KC_5,  KC_6,  KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,  KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+		KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,    KC_5,  KC_6,  KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,  KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,  KC_H,  KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, LGUI_T(KC_ENT),
 		KC_LSPO, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,  KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
-		_______, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), TG(3), MO(2)
+		MO(4),   KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), TG(3), MO(2)
 		),
 	[1] = LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
@@ -33,5 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		XXXXXXX, KC_PPLS, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		KC_P0,   KC_PMNS, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		KC_BSPC, KC_DEL,  XXXXXXX, KC_PENT, XXXXXXX, XXXXXXX, _______, XXXXXXX
+		),
+	[4] = LAYOUT_60_ansi(
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, S(KC_BSLS),
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______
 		)
 };
