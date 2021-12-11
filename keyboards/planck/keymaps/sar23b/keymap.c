86d0ae1dec702606d1cb04915e0505893c806e84
sar <git.sar@coors-online.de>
2021-12-11 17:09:50 +0100

    planck > split spacebar

--- keyboards/planck/keymaps/sar23b/keymap.c
+++ keyboards/planck/keymaps/sar23b/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Play |  GUI | Esc  | Alt  |Lower |    Space    |Raise | AltG | Mouse| Num  | Adju |
+ * | Play |  GUI | Esc  | Alt  |Lower | Bksp | Spce |Raise | AltG | Mouse| Num  | Adju |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_planck_1x2uC( 
+[_QWERTY] = LAYOUT_ortho_4x12( 
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SUPENT,
     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-    KC_MPLY, KC_LGUI, KC_ESC,  KC_LALT, LOWER,   KC_SPC,           RAISE,   KC_RALT, MOUSE,   NUMPAD,  ADJUST
+    KC_MPLY, KC_LGUI, KC_ESC,  KC_LALT, LOWER,   KC_BSPC, KC_SPC,  RAISE,   KC_RALT, MOUSE,   NUMPAD,  ADJUST
 ),
 
 /* Lower
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   \  |   {  |   }  |   ~  | XXXX | XXXX |   "  | XXXX | XXXX | XXXX |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX |      | XXXX |      |      |      `      |      |      | XXXX | XXXX | XXXX |
+ * | XXXX |      | XXXX |      |      |   `  |   `  |      |      | XXXX | XXXX | XXXX |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_planck_1x2uC( //missing keys
+[_LOWER] = LAYOUT_ortho_4x12( //missing keys
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     _______, KC_QUOT, KC_LBRC, KC_RBRC, KC_UNDS, KC_PIPE, XXXXXXX, KC_EQL,  KC_PLUS, KC_MINS, XXXXXXX, XXXXXXX,
     _______, KC_BSLS, KC_LCBR, KC_RCBR, KC_TILD, XXXXXXX, XXXXXXX, KC_DQUO, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    XXXXXXX, _______, XXXXXXX, _______, _______, KC_GRV,           _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, _______, XXXXXXX, _______, _______, KC_GRV,  KC_GRV,  _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Raise
@@ -58,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Vol- | Vol+ | Mute | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | Disp+| Disp-|      |      |     XXXX    |      | XXXX | XXXX | XXXX |      |
+ * | XXXX | Disp+| Disp-|      |      | XXXX | XXXX |      | XXXX | XXXX | XXXX |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_planck_1x2uC( //navigation
+[_RAISE] = LAYOUT_ortho_4x12( //navigation
     XXXXXXX, KC_MPRV, KC_UP,   KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_LGUI,
     _______, KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    XXXXXXX, KC_BRID, KC_BRIU, _______, _______, XXXXXXX,          _______, XXXXXXX, XXXXXXX, XXXXXXX, _______
+    XXXXXXX, KC_BRID, KC_BRIU, _______, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______
 ),
 
 /* Keep (Lower + Raise)
@@ -76,14 +76,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F9  |  F10 |  F11 |  F12 | XXXX | XXXX |  Ins |  End | pDown| XXXX |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX |      | XXXX |      |      |     XXXX    |      |      | XXXX | XXXX | XXXX |
+ * | XXXX |      | XXXX |      |      | XXXX | XXXX |      |      | XXXX | XXXX | XXXX |
  * `-----------------------------------------------------------------------------------'
  */
-[_KEEP] = LAYOUT_planck_1x2uC(
+[_KEEP] = LAYOUT_ortho_4x12(
     XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_DEL,  KC_HOME, KC_PGUP, XXXXXXX, KC_PSCR,
     _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, KC_INS,  KC_END,  KC_PGDN, XXXXXXX, _______,
-    XXXXXXX, _______, XXXXXXX, _______, _______, XXXXXXX,          _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, _______, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Adjust 
@@ -94,14 +94,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | Hue- | Sat- | Brt- | Spd- | XXXX | Power|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX | XXXX |     RGB     |      | AUD1 | AUD0 | Reset|      |
+ * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | RGB  |      | AUD1 | AUD0 | Reset|      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_planck_1x2uC( //keeb control
+[_ADJUST] = LAYOUT_ortho_4x12( //keeb control
     MU_ON,   LIGHT,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_RMOD, XXXXXXX, XXXXXXX, KC_SLEP,
     GAME,    SONG,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUI, RGB_SAI, RGB_VAI,  RGB_SPI, XXXXXXX, KC_WAKE,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,  RGB_SPD, XXXXXXX, KC_PWR,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,          _______, AU_ON,    AU_OFF,  RESET,   _______
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, RGB_TOG, _______, AU_ON,    AU_OFF,  RESET,   _______
 ),
 
 /* Numpad
@@ -112,14 +112,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Bksp |   /  |   1  |   2  |   3  |   -  | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Lock | XXXX | XXXX |   .  |   0  |     Ent     | XXXX | XXXX | XXXX | Exit | XXXX |
+ * | Lock | XXXX | XXXX |   .  |   0  |  Ent |  Ent | XXXX | XXXX | XXXX | Exit | XXXX |
  * `-----------------------------------------------------------------------------------'
  */
-[_NUMPAD] = LAYOUT_planck_1x2uC( 
+[_NUMPAD] = LAYOUT_ortho_4x12( 
     KC_TAB,  XXXXXXX, KC_P7,   KC_P8,   KC_P9,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_BSPC, KC_PSLS, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_NLCK, XXXXXXX, XXXXXXX, KC_PDOT, KC_P0,   KC_PENT,          XXXXXXX, XXXXXXX, XXXXXXX, EXT_NUM, XXXXXXX
+    KC_NLCK, XXXXXXX, XXXXXXX, KC_PDOT, KC_P0,   KC_PENT, KC_PENT, XXXXXXX, XXXXXXX, XXXXXXX, EXT_NUM, XXXXXXX
 ),
 
 /* Mouse 
@@ -130,14 +130,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |  M4  |  M5  | CUT  | COPY | PASTE|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX | XXXX |     XXXX    |  M1  | Acl3 | Exit | Acl1 | Acl2 |
+ * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |  M1  | Acl3 | Exit | Acl1 | Acl2 |
  * `-----------------------------------------------------------------------------------'
  */
-[_MOUSE] = LAYOUT_planck_1x2uC( //mouse layer 
+[_MOUSE] = LAYOUT_ortho_4x12( //mouse layer 
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN3, KC_MS_U, KC_BTN2, KC_WH_U, KC_WH_L,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_WH_R,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, CUT,     COPY,    PASTE,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_BTN1, KC_ACL0, EXT_MSE, KC_ACL1, KC_ACL2
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_ACL0, EXT_MSE, KC_ACL1, KC_ACL2
 ),
 
 /* Unicode
@@ -148,14 +148,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  ZWJ |  🏳  |  💅  |  🌨  |  😶  |  🐍  | YYYY |  👾  |  🌌  |  🔥  |  🌆  | YYYY |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift| XXXX | XXXX | XXXX | XXXX |    🐬/🐋    |      | XXXX | XXXX |  🌇  |      |
+ * | Shift| XXXX | XXXX | XXXX | XXXX |  🐬  |  🐋  |      | XXXX | XXXX |  🌇  |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_UNICODE] = LAYOUT_planck_1x2uC( //unicode char layer
-    XXXXXXX, X(TRANS), X(GLITT),  X(UNICRN), X(CATPIL), X(PENGU),  XXXXXXX, X(TURTLE), X(SATURN), X(NISTA), X(NBRI),   KC_BSPC,
-    X(VS16), X(GAY),   X(PHEART), X(FLKE),   X(UPDF),   X(SAUWIA), X(GIFT), X(BUTFLY), X(SAT),    X(STARS), X(FIRWRK), KC_ENT,
-    X(ZWJ),  X(FLAG),  X(NAILS),  X(SNOW),   X(NMOF),   X(SNEK),   XXXXXXX, X(ALIEN),  X(MIKWAY), X(LIT),   X(CDUSK),  XXXXXXX,
-    KC_LSFT, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XP(DOLPI,WHALE),    _______,   XXXXXXX,   XXXXXXX,  X(SUNST),  _______
+[_UNICODE] = LAYOUT_ortho_4x12( //unicode char layer
+    XXXXXXX, X(TRANS), X(GLITT),  X(UNICRN), X(CATPIL), X(PENGU),  XXXXXXX,  X(TURTLE), X(SATURN), X(NISTA), X(NBRI),   KC_BSPC,
+    X(VS16), X(GAY),   X(PHEART), X(FLKE),   X(UPDF),   X(SAUWIA), X(GIFT),  X(BUTFLY), X(SAT),    X(STARS), X(FIRWRK), KC_ENT,
+    X(ZWJ),  X(FLAG),  X(NAILS),  X(SNOW),   X(NMOF),   X(SNEK),   XXXXXXX,  X(ALIEN),  X(MIKWAY), X(LIT),   X(CDUSK),  XXXXXXX,
+    KC_LSFT, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   X(DOLPI),  X(WHALE), _______,   XXXXXXX,   XXXXXXX,  X(SUNST),  _______
 ),
 
 /* Music
@@ -166,14 +166,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX | XXXX |     XXXX    | XXXX | XXXX | XXXX | XXXX | XXXX |
+ * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * `-----------------------------------------------------------------------------------'
  */
-[_MUSIC] = LAYOUT_planck_1x2uC( //Music map layer
+[_MUSIC] = LAYOUT_ortho_4x12( //Music map layer
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MSCTRL,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Music control
@@ -184,14 +184,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX |  REC |     STOP    | PLAY | FAST | SLOW | XXXX | XXXX |
+ * | XXXX | XXXX | XXXX | XXXX |  REC | STOP | STOP | PLAY | FAST | SLOW | XXXX | XXXX |
  * `-----------------------------------------------------------------------------------'
  */
-[_MUSCTL] = LAYOUT_planck_1x2uC( //Music map control layer
+[_MUSCTL] = LAYOUT_ortho_4x12( //Music map control layer
     MU_OFF,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MU_MOD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_LALT,          KC_LGUI, KC_UP,   KC_DOWN, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, KC_UP,   KC_DOWN, XXXXXXX, XXXXXXX
 ),
 
 /* Game 
@@ -202,14 +202,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | XXXX | Shift|   Z  |   X  |   C  |   V  | XXXX |  Ins |  End | pDown| XXXX | Next |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | Caps | XXXX | XXXX | Alt  |     Space   | XXXX | XXXX | XXXX | XXXX | Exit |
+ * | XXXX | Caps | XXXX | XXXX | Alt  | Spce | Spce | XXXX | XXXX | XXXX | XXXX | Exit |
  * `-----------------------------------------------------------------------------------'
  */
-[_GAME] = LAYOUT_planck_1x2uC( //layout for gaming
+[_GAME] = LAYOUT_ortho_4x12( //layout for gaming
     KC_ESC,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY,
     XXXXXXX, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   XXXXXXX, KC_DEL,  KC_HOME, KC_PGUP, XXXXXXX, KC_MPRV,
     XXXXXXX, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   XXXXXXX, KC_INS,  KC_END,  KC_PGDN, XXXXXXX, KC_MNXT,
-    XXXXXXX, KC_CAPS, XXXXXXX, XXXXXXX, KC_LALT, KC_SPC,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EXT_GME 
+    XXXXXXX, KC_CAPS, XXXXXXX, XXXXXXX, KC_LALT, KC_SPC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EXT_GME 
 ),
 
 /* RGB light test mode 
@@ -220,14 +220,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX | XXXX |     XXXX    | XXXX | XXXX | XXXX | XXXX | Exit |
+ * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | Exit |
  * `-----------------------------------------------------------------------------------'
  */
-[_LIGHT] = LAYOUT_planck_1x2uC( // mode to test out colors
+[_LIGHT] = LAYOUT_ortho_4x12( // mode to test out colors
     XXXXXXX, S_DYELLOW, S_LBLUE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       S_ORANGE, S_GOLDENROD, S_GOLD,  S_AZURE,      S_OFF,
     XXXXXXX, S_BWHITE,  S_CWHITE, XXXXXXX, XXXXXXX, XXXXXXX, S_RED,         S_PINK,   S_GREEN,     S_CYAN,  S_BLUE,       XXXXXXX,
     XXXXXXX, S_GWHITE,  S_CGREEN, XXXXXXX, XXXXXXX, XXXXXXX, S_SPRINGGREEN, S_PURPLE, S_MAGENTA,   S_CORAL, S_CHARTREUSE, XXXXXXX,
-    XXXXXXX, XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,                S_TEAL,   S_TURQUOISE, S_WHITE, S_YELLOW,     EXT_LGT 
+    XXXXXXX, XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       S_TEAL,   S_TURQUOISE, S_WHITE, S_YELLOW,     EXT_LGT 
 ),
 
 /* Song test mode 
@@ -238,14 +238,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX | XXXX |     XXXX    | XXXX | XXXX | XXXX | XXXX | Exit |
+ * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | Exit |
  * `-----------------------------------------------------------------------------------'
  */
-[_SONG] = LAYOUT_planck_1x2uC( // testing songs
+[_SONG] = LAYOUT_ortho_4x12( // testing songs
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EXT_SNG
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EXT_SNG
 )
 
 /* Layout 
@@ -256,14 +256,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | XXXX | XXXX | XXXX | XXXX | XXXX |     XXXX    | XXXX | XXXX | XXXX | XXXX | XXXX |
+ * | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX | XXXX |
  * `-----------------------------------------------------------------------------------'
  * /
-[_LAYER] = LAYOUT_planck_1x2uC( //generic layout
+[_LAYER] = LAYOUT_ortho_4x12( //generic layout
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 */
 };
